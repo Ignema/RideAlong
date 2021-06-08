7eb7292af6cad4de850db1a1c17858d10cf42ed4
DOUSLIMI Yasser <ignema5@hotmail.com>
2021-06-08 18:20:26 +0100

    Updated fake data with fake passwords

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO
-    participant (participant_id, name, phone, email, birthday)
+    participant (participant_id, name, phone, email, birthday, password)
 VALUES
-       (nextval('participant_sequence'), 'john', 0000, 'john@test', '1999-03-14'),
-       (nextval('participant_sequence'), 'jack', 0001, 'jack@test', '1958-08-11'),
-       (nextval('participant_sequence'), 'sophie', 0002, 'sophie@test', '2000-04-19'),
-       (nextval('participant_sequence'), 'dan', 0003, 'dan@test', '1975-03-28'),
-       (nextval('participant_sequence'), 'tim', 0004, 'tim@test', '1989-01-11'),
-       (nextval('participant_sequence'), 'linda', 0005, 'linda@test', '2002-08-14'),
-       (nextval('participant_sequence'), 'peter', 0006, 'peter@test', '1997-12-18'),
-       (nextval('participant_sequence'), 'tom', 0007, 'tom@test', '2002-03-26');
+       (nextval('participant_sequence'), 'john', 0000, 'john@test', '1999-03-14', '12345'),
+       (nextval('participant_sequence'), 'jack', 0001, 'jack@test', '1958-08-11', '12345'),
+       (nextval('participant_sequence'), 'sophie', 0002, 'sophie@test', '2000-04-19', '12345'),
+       (nextval('participant_sequence'), 'dan', 0003, 'dan@test', '1975-03-28', '12345'),
+       (nextval('participant_sequence'), 'tim', 0004, 'tim@test', '1989-01-11', '12345'),
+       (nextval('participant_sequence'), 'linda', 0005, 'linda@test', '2002-08-14', '12345'),
+       (nextval('participant_sequence'), 'peter', 0006, 'peter@test', '1997-12-18', '12345'),
+       (nextval('participant_sequence'), 'tom', 0007, 'tom@test', '2002-03-26', '12345');
 
 INSERT INTO
     meetup (meetup_id, completed, date, gallery, location)
